80b6ded06554b7d67f6f11de4a216e158a9110d8
wby3210 <51739277+wby3210@users.noreply.github.com>
2022-10-17 21:02:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -29,35 +29,31 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '*生日', name: '宝贝', year: '1995', date: '08-27',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '10-02',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
+      //customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        //{ keyword: 'love_day', date: '2022-09-08' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
+        //{ keyword: 'marry_day', date: '2022-09-09' },
+      //],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'c-lNF71u4_0FrKGbiAWlLctJdX0_bUEE5W24czt6vlA',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oGjoU6ZqaJwrZBrkWz66wIvtEO4I',
     }
   ],
 
