b4c6bcbdef82eaa8496035da7afecbb4b61abeca
wby3210 <51739277+wby3210@users.noreply.github.com>
2022-10-17 21:49:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '朵朵',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oGjoU6ZqaJwrZBrkWz66wIvtEO4I',
+      id: 'oGjoU6VUxa7GkROZZfGi09KdbVGc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'M-Nq4uBxLhV76Gdr1ssC6JUr6zuARku0d65Eul9cWgM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
